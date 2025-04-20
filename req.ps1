83cfe5087f8e343d597943a5ad7f925fa1c97e4e
Updated form Workflow <carstenriedel@outlook.com>
2025-04-20 07:28:29 +0200

    update

--- req.ps1
+++ req.ps1
@@ -3,7 +3,11 @@
 Write-Host "Starting script execution ..."
 
 # Ensure the current user can run local scripts that are remotely signed
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -Verbose
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 # Updated second statement—now includes the complete switch for clarity
 Write-Host "Execution policy (-ExecutionPolicy RemoteSigned) set successfully."
+
+Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 -Scope CurrentUser | Out-Null
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module PowerShellGet -Force -Scope CurrentUser -AllowClobber -WarningAction SilentlyContinue | Out-Null
