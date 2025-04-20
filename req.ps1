69a1ab0aa37001af72a29241d9c09d97b82bb46f
Updated form Workflow <carstenriedel@outlook.com>
2025-04-20 07:05:32 +0200

    update

--- req.ps1
+++ req.ps1
@@ -1,3 +1,10 @@
 Write-Output "Hello, World!"
 Write-Output "This is a PowerShell script."
-Install-Module -Name BlackBytesBox.Manifested.Initialize -Scope CurrentUser
+
+
+powershell -NoProfile -ExecutionPolicy Unrestricted -Command "& {
+    Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 -Scope CurrentUser | Out-Null;
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
+    Install-Module PowerShellGet -Force -Scope CurrentUser -AllowClobber -WarningAction SilentlyContinue | Out-Null;
+    Install-Module -Name BlackBytesBox.Manifested.Initialize -Scope CurrentUser -AllowClobber -Force -Repository PSGallery;
+}" ; exit
