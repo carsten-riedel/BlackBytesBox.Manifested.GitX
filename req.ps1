15c99973ffd19040789b0bbc6e153834cf52ee22
Updated form Workflow <carstenriedel@outlook.com>
2025-04-21 10:34:12 +0200

    update

--- req.ps1
+++ req.ps1
@@ -544,15 +544,3 @@ if (Get-Command python -ErrorAction SilentlyContinue) {
 
 
 
-
-
-# Ensure the current user can run local scripts that are remotely signed
-#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
-# Updated second statement—now includes the complete switch for clarity
-#Write-Host "Execution policy (-ExecutionPolicy RemoteSigned) set successfully."
-
-#Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 -Scope CurrentUser | Out-Null
-#Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-#Install-Module PowerShellGet -Force -Scope CurrentUser -AllowClobber -WarningAction SilentlyContinue | Out-Null
-#Install-Module -Name BlackBytesBox.Manifested.Initialize -Scope CurrentUser -AllowClobber -Force -Repository PSGallery
